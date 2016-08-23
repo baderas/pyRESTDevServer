2904455cf892f82e9bcdd85c6945095635075813
Andreas Bader <Development@Geekparadise.de>
2016-08-23 15:55:10 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pyRESTDevServer
-A small python server that helps developing/debugging a REST client.
+A small python server that helps developing/debugging a REST client. It simply prints out every received POST request.
+
+## Requirements
+* Python3
+* Flask
+  * install with pip: `pip install flask`
+  * or use your distributions package, e.g. Debian package: `sudo apt-get install python-flask`
+  
+## Usage
+* Start Server: `./pyRESTDevServer.py`
+* Test with curl: `curl -i -H "Content-Type: application/json" -X POST -d @test.json http://localhost:5000/write`
---
+++ pyRESTDevServer.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Development REST Server
+# Written in Python 2
+# Uses Flask
+#     Debian package: python-flask
+#     Pip: flask
+# Author: Andreas Bader
+# https://github.com/baderas/pyRESTDevServer
+
+# Takes every POST Request and prints it out
+
+# Usage:
+# Start Server:
+#    ./pyRESTDevServer.py
+# Test with curl:
+#    curl -i -H "Content-Type: application/json" -X POST -d @test.json http://localhost:5000/write
+
+__author__ = 'Andreas Bader'
+__version__ = "0.01"
+
+import flask
+import logging
+
+conn = None
+
+app = flask.Flask(__name__)
+
+# Configure Logging
+logLevel = logging.DEBUG
+logger = logging.getLogger(__name__)
+logger.setLevel(logLevel)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+handler.setLevel(logLevel)
+logger.addHandler(handler)
+
+@app.route('/write', methods=['POST'])
+def handle_write():
+    logger.info("Got a POST request.")
+    if flask.request.json:
+        body = flask.request.get_json()
+        logger.info("Got a json body with the following content:")
+        logger.info(flask.json.dumps(body))
+    return "",200 # 200 stands for "OK", use 201 for "CREATED"
+
+@app.errorhandler(404)
+def not_found(error):
+    return  flask.make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+     app.run(debug=True) 
+
+logger.info("End.")
